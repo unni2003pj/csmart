bf180c80403cb58ea05ab26082207799603cb1bc
unnikrishnan <unnikrishnan@logicalsteps.net>
2023-10-28 17:32:12 +0530

    up

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
       $(this).closest('.header-inner').find(".menu-panel").toggleClass('active');    
     });
 
+    // Animation 3D
 
     let preserve = document.querySelector('.banner-area');
 
@@ -23,89 +24,6 @@ $(document).ready(function(){
 
     // Banner section
     
-    // var sync1 = $("#sync1");
-    // var sync2 = $("#sync2");
-    // var slidesPerPage = 5;
-    // var syncedSecondary = true;
-
-    
-    // if ($(document).width() < 767) {
-    //     var slidesPerPage = 1;
-    // } else if  ($(document).width() <= 1024) {
-    //     var slidesPerPage = 2;
-    // }
-
-    // sync1.owlCarousel({
-    //     items: 1,
-    //     slideSpeed: 2000,
-    //     nav: true,
-    //     autoplay: true, 
-    //     dots: true,
-    //     loop: true,
-    //     responsiveRefreshRate: 200,
-    // }).on('changed.owl.carousel', syncPosition);
-
-    // sync2
-    // .on('initialized.owl.carousel', function() {
-    //     sync2.find(".owl-item").eq(0).addClass("current");
-    // })
-    // .owlCarousel({
-    //     items: slidesPerPage,
-    //     dots: false,
-    //     nav: false,
-    //     margin: 36,
-    //     stagePadding: 18,
-    //     smartSpeed: 200,
-    //     slideSpeed: 500,
-    //     slideBy: slidesPerPage,
-    //     responsiveRefreshRate: 100
-    // }).on('changed.owl.carousel', syncPosition2);
-
-    // function syncPosition(el) {
-       
-    //     var count = el.item.count - 1;
-    //     var current = Math.round(el.item.index - (el.item.count / 2) - .5);
-
-    //     if (current < 0) {
-    //         current = count;
-    //     }
-    //     if (current > count) {
-    //         current = 0;
-    //     }
-
-        
-
-    //     sync2
-    //         .find(".owl-item")
-    //         .removeClass("current")
-    //         .eq(current)
-    //         .addClass("current");
-    //     var onscreen = sync2.find('.owl-item.active').length - 1;
-    //     var start = sync2.find('.owl-item.active').first().index();
-    //     var end = sync2.find('.owl-item.active').last().index();
-
-    //     if (current > end) {
-    //         sync2.data('owl.carousel').to(current, 100, true);
-    //     }
-    //     if (current < start) {
-    //         sync2.data('owl.carousel').to(current - onscreen, 100, true);
-    //     }
-    // }
-
-    // function syncPosition2(el) {
-    //     if (syncedSecondary) {
-    //         var number = el.item.index;
-    //         sync1.data('owl.carousel').to(number, 100, true);
-    //     }
-    // }
-
-    // sync2.on("click", ".owl-item", function(e) {
-    //     e.preventDefault();
-    //     var number = $(this).index();
-    //     sync1.data('owl.carousel').to(number, 300, true);
-    // });
-
-
     $('.hero-carousel-slider').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -129,12 +47,13 @@ $(document).ready(function(){
         focusOnSelect: true,
         infinite: true,
         arrows: false,
+        speed: 400,
         asNavFor: '.hero-carousel-slider',
         responsive: [{
           breakpoint: 1024,
           settings: {
-            slidesToShow: 5,
-            slidesToScroll: 5,
+            slidesToShow: 3,
+            slidesToScroll: 1,
           }
         }, {
           breakpoint: 640,
@@ -160,26 +79,9 @@ $(document).ready(function(){
    
     $('.carousel-card-slider').on('click', '.slick-slide', function(event) {
       event.preventDefault();
-      var goToSingleSlide = $(this).data('slick-index');
-   
+      var goToSingleSlide = $(this).data('slick-index');   
       $('.hero-carousel-slider').slick('slickGoTo', goToSingleSlide);
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // client slider
 
@@ -190,7 +92,7 @@ $(document).ready(function(){
         speed: 100,
         slidesToShow: 4,
         pauseOnHover:true,
-        centerMode: true,
+        centerMode: false,
         slidesToScroll: 1,
         variableWidth: true,
         prevArrow: false,
@@ -265,7 +167,7 @@ $(document).ready(function(){
         autoplay: true,
         dots: false,
         infinite: true,
-        speed: 300,
+        speed: 400,
         slidesToShow: 3,
         slidesToScroll: 1,
         centerMode: false,
